f6c7c84e5f2c447a36efa416fdd698da31f048b8
Fede <fedemc@gmail.com>
2018-07-01 20:53:34 -0300

    Added EditorSnap

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+    [SerializeField][Tooltip("Size of the grid")] [Range(1,10)] int gridSize=10;
+    void Update()
+    {
+        Vector3 snapPosition;
+        snapPosition.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPosition.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+        transform.position = new Vector3(snapPosition.x, 0f, snapPosition.z);
+    }
+}
