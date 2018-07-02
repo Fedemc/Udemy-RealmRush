b2638d3f935d2ea75df07a2ad8521d8bac20e666
Fede <fedemc@gmail.com>
2018-07-01 22:14:40 -0300

    Added font and textmesh
    Grid coordinates are shown in each cube depending on its oposition.

--- Assets/Scripts/CubeEditor.cs
+++ Assets/Scripts/CubeEditor.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
 {
     [SerializeField][Tooltip("Size of the grid")] [Range(1,10)] int gridSize=10;
+    TextMesh textMesh;
+
+    private void Start()
+    {
+        textMesh = GetComponentInChildren<TextMesh>();
+    }
+
     void Update()
     {
         Vector3 snapPosition;
         snapPosition.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPosition.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
         transform.position = new Vector3(snapPosition.x, 0f, snapPosition.z);
+
+        textMesh.text = snapPosition.x / gridSize + "," + snapPosition.z / gridSize;
     }
 }
