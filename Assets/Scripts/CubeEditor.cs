e534687d31decc1ee7608cb7f37f08dadb716dca
Fede <fedemc@gmail.com>
2018-07-09 21:55:05 -0300

    Decoupled CubeEditor
    CubeEditor now depends of Waypoint

--- Assets/Scripts/CubeEditor.cs
+++ Assets/Scripts/CubeEditor.cs
@@ -4,23 +4,37 @@ using UnityEngine;
 
 [ExecuteInEditMode]
 [SelectionBase]
+[RequireComponent(typeof(Waypoint))]
 public class CubeEditor : MonoBehaviour
 {
-    [SerializeField][Tooltip("Size of the grid")] [Range(1,10)] int gridSize=10;
-    TextMesh textMesh;
+    Waypoint waypoint;
 
-    private void Start()
+    private void Awake()
     {
-        textMesh = GetComponentInChildren<TextMesh>();
+        waypoint = GetComponent<Waypoint>();
     }
+
 
     void Update()
     {
-        Vector3 snapPosition;
-        snapPosition.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
-        snapPosition.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
-        transform.position = new Vector3(snapPosition.x, 0f, snapPosition.z);
-        string labelText = snapPosition.x / gridSize + "," + snapPosition.z / gridSize;
+        SnapToGrid();
+        UpdateLabel();
+    }
+
+    private void SnapToGrid()
+    {
+        int gridSize = waypoint.GetGridSize();
+        transform.position = new Vector3(
+            waypoint.GetGridPos().x,
+            0f,
+            waypoint.GetGridPos().y);
+    }
+
+    private void UpdateLabel()
+    {
+        TextMesh textMesh=GetComponentInChildren<TextMesh>();
+        int gridSize = waypoint.GetGridSize();
+        string labelText = waypoint.GetGridPos().x/ gridSize + "," + waypoint.GetGridPos().y / gridSize;
         textMesh.text = labelText;
         gameObject.name = labelText;
     }
