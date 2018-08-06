271a55943d15d32a940f6bc8101a063569b73af6
Fede <fedemc@gmail.com>
2018-08-05 22:03:49 -0300

    Test stage designed. Towers added

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] Transform objectToPan;
+    [SerializeField] Transform targetEnemy;
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookAtEnemy(targetEnemy);
+	}
+
+    private void LookAtEnemy(Transform targetEnemy)
+    {
+        objectToPan.LookAt(targetEnemy);
+    }
+}
