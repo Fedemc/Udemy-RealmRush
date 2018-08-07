cf71950ba8d32ae1e41a64d6cd6e39706c9ce422
Fede <fedemc@gmail.com>
2018-08-07 00:00:50 -0300

    Throwing fruit on shoot at target

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -7,12 +7,29 @@ public class Tower : MonoBehaviour
 {
     [SerializeField] Transform objectToPan;
     [SerializeField] Transform targetEnemy;
+    [SerializeField] float distanceToShoot;
+    [SerializeField] GameObject towerBullet;
 
     // Update is called once per frame
     void Update()
     {
         LookAtEnemy(targetEnemy);
-	}
+        CalculateDistance();
+    }
+
+    private void CalculateDistance()
+    {
+        if (Vector3.Distance(gameObject.transform.position, targetEnemy.position) <= distanceToShoot)
+        {
+            ShootEnemy();
+        }
+    }
+
+    private void ShootEnemy()
+    {
+        Instantiate(towerBullet, transform.position, transform.rotation);
+        towerBullet.GetComponent<Rigidbody>().velocity = 1*(targetEnemy.position - towerBullet.transform.position);
+    }
 
     private void LookAtEnemy(Transform targetEnemy)
     {
