f793a49283768ee84181596deb9053236e62f6d2
Fede <fedemc@gmail.com>
2018-08-09 22:24:51 -0300

    2 custom towers and particles as bullets

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -14,21 +14,15 @@ public class Tower : MonoBehaviour
     void Update()
     {
         LookAtEnemy(targetEnemy);
-        CalculateDistance();
+        //CalculateDistance();
     }
 
     private void CalculateDistance()
     {
         if (Vector3.Distance(gameObject.transform.position, targetEnemy.position) <= distanceToShoot)
         {
-            ShootEnemy();
+            
         }
-    }
-
-    private void ShootEnemy()
-    {
-        Instantiate(towerBullet, transform.position, transform.rotation);
-        towerBullet.GetComponent<Rigidbody>().velocity = 1*(targetEnemy.position - towerBullet.transform.position);
     }
 
     private void LookAtEnemy(Transform targetEnemy)
